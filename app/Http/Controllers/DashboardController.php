73c089e39c3489b8cfe08358b324f913b5909c2d
Touhid <aoradev04@gmail.com>
2023-07-09 00:02:08 +0600

    Updated Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,9 +9,9 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        $total_owner = AddNewHouse::whereNull('deleted_at')->count();
-        $total_customer = User::whereNull('deleted_at')->where('user_type',1)->count();
-        $total_houses = User::whereNull('deleted_at')->where('user_type',3)->count();
+        $total_houses = AddNewHouse::whereNull('deleted_at')->count();
+        $total_owner = User::whereNull('deleted_at')->where('user_type',2)->count();
+        $total_customer = User::whereNull('deleted_at')->where('user_type',3)->count();
         $total_booked_house = AddNewHouse::whereNull('deleted_at')->where('booked_status',1)->count();
         //dd($total_houses);
         $data = [
