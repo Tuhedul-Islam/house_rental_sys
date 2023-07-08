2702549c2ac54327b3cf814a470946f6740df623
Touhid <aoradev04@gmail.com>
2023-07-08 23:32:09 +0600

    Dashboard update

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -26,7 +26,7 @@
         <div class="col-lg-3 col-6">
             <div class="small-box bg-success">
                 <div class="inner">
-                    <h3>{{ $total_owner??0 }}<sup style="font-size: 20px">%</sup></h3>
+                    <h3>{{ $total_owner??0 }}<sup style="font-size: 20px"></sup></h3>
                     <p>Total Owner</p>
                 </div>
                 <div class="icon">
@@ -47,13 +47,12 @@
                 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
-        <!--
+        <!---->
         <div class="col-lg-3 col-6">
             <div class="small-box bg-danger">
                 <div class="inner">
-                    <h3>65</h3>
-
-                    <p>Unique Visitors</p>
+                    <h3>{{ $total_booked_house??'0' }}</h3>
+                    <p>Total Booked</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-pie-graph"></i>
@@ -61,7 +60,6 @@
                 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
-        -->
     </div>
 @stop
 
