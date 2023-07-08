30b23210d8c9a5c91ee82d2e5a6bdc54069eb9cb
Touhid <aoradev04@gmail.com>
2023-07-08 22:30:34 +0600

    Update Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AddNewHouse;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(){
-
-        return view('backend.dashboard');
+        $total_owner = AddNewHouse::whereNull('deleted_at')->count();
+        $total_customer = AddNewHouse::whereNull('deleted_at')->count();
+        $total_houses = AddNewHouse::whereNull('deleted_at')->count();
+        //dd($total_houses);
+        $data = [
+            'total_owner' => $total_owner,
+            'total_customer' => $total_customer,
+            'total_houses' => $total_houses,
+        ];
+        return view('backend.dashboard', $data);
     }
 }
